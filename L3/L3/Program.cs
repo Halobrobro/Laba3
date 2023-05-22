910aca85297e15068ee5610eb561196979ea2df6
{Halobrobro} <{Halobrobro@mail.ru}>
2023-05-22 13:53:47 +0700

    -

--- L3/L3/Program.cs
+++ L3/L3/Program.cs
@@ -7,21 +7,18 @@ namespace Menu
         {
 
 
-            double a;
-            double b;
             char c;
             double d;
-            Console.WriteLine("Введите А: ");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите B: ");
-            b = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine("Введите знак");
-            Console.WriteLine("+");
             Console.WriteLine("-");
-            Console.WriteLine("*");
-            Console.WriteLine("/");
+
             c = Convert.ToChar(Console.ReadLine());
-           
+            if (c == '-')
+            {
+                d = a - b;
+            }
+
 
         }
     }
