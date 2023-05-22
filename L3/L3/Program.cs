ad3760dbf721e60e367a801a08425afa1bf8190e
{Halobrobro} <{Halobrobro@mail.ru}>
2023-05-22 13:42:03 +0700

    1

---
+++ L3/L3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double a;
+            double b;
+            char c;
+            double d;
+            Console.WriteLine("Введите А: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите B: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите знак");
+            Console.WriteLine("+");
+            Console.WriteLine("-");
+            Console.WriteLine("*");
+            Console.WriteLine("/");
+            c = Convert.ToChar(Console.ReadLine());
+           
+
+        }
+    }
+}
