9f25d019c3ab58682d8d279d1ef2a4d432fb57c6
{Halobrobro} <{Halobrobro@mail.ru}>
2023-05-22 13:48:06 +0700

    A

--- L3/L3/Program.cs
+++ L3/L3/Program.cs
@@ -8,21 +8,8 @@ namespace Menu
 
 
             double a;
-            double b;
-            char c;
-            double d;
             Console.WriteLine("Введите А: ");
             a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите B: ");
-            b = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите знак");
-            Console.WriteLine("+");
-            Console.WriteLine("-");
-            Console.WriteLine("*");
-            Console.WriteLine("/");
-            c = Convert.ToChar(Console.ReadLine());
-           
-
         }
     }
 }
